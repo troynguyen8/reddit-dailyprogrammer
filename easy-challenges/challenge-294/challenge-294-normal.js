54df26dc70bff7ecc00e687463738f11c9fe1140
troynguyen8 <troynguyen8@gmail.com>
2016-12-17 15:05:32 -0600

    Add files via upload

--- easy-challenges/challenge-294/challenge-294-normal.js
+++ easy-challenges/challenge-294/challenge-294-normal.js
@@ -4,3 +4,21 @@
  * reddit.com/r/dailyprogrammer easy challenge #294: Rack management 1 normal
  */
 
+var input = [['ladilmy', 'daily'], ['eerriin', 'eerie'], ['orrpgma', 'program'], ['orppgma', 'program']];
+
+function scrabble(letterTiles, desiredWord) {
+	for(var j = 0; j < desiredWord.length; j++) {
+		if(!(letterTiles.includes(desiredWord.charAt(j)))) {
+			return false;
+		}
+		
+		letterTiles = letterTiles.replace(desiredWord.charAt(j), '');
+	}
+	
+	console.log(letterTiles);
+	return true;
+}
+
+for(var i = 0; i < input.length; i++) {
+	console.log(scrabble(input[i][0], input[i][1]));
+}
